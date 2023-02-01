5914da76ea0176865d8c12a67d8b9cde64b5fd5b
MORGAN MEZAACHE <morgan.mezaache.pro@gmail.comgit config --global user.name MORGAN>
2023-02-01 09:18:38 +0100

    test

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,39 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SET FOREIGN_KEY_CHECKS = 0;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+DROP TABLE IF EXISTS `club`;
+
+CREATE TABLE `club` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `sport` varchar(255),
+  `trainer` varchar(255)
+);
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `class` varchar(255),
+  `email` varchar(255) NOT NULL,
+  `hashedPassword` varchar(255) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT 0,
+  `creationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `club_id` int,
+FOREIGN KEY (`club_id`) REFERENCES `club`(`id`)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+INSERT INTO user (firstname, lastname, email, hashedPassword, admin) VALUES ('Morgan', 'Mezaache', "mezaache.morgan@gmail.com", '', 1);
+
+DROP TABLE IF EXISTS `inscription`;
+
+CREATE TABLE `inscription` (
+  user_id int NOT NULL,
+  club_id int NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  FOREIGN KEY (club_id) REFERENCES club(id)
+);
+
+SET FOREIGN_KEY_CHECKS = 1;
